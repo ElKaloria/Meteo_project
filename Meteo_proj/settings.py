84750c607fadd3ca4f60b4ba2dbd49e30e857b58
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-22 10:53:59 +0700

    add drf

--- Meteo_proj/settings.py
+++ Meteo_proj/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "meteo_app.apps.MeteoAppConfig"
+    'meteo_app.apps.MeteoAppConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
