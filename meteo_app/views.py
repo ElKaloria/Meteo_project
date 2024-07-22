b8ca38224e31f6fb45943c8463420e095a0f7490
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-22 10:52:52 +0700

    add user history in view function and template

--- meteo_app/views.py
+++ meteo_app/views.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 import numpy as np
 from django.shortcuts import render, reverse
 from .forms import MeteoForm
@@ -6,6 +9,8 @@ import openmeteo_requests
 from retry_requests import retry
 import pandas as pd
 import requests
+
+from .models import UserHistory
 
 
 # Create your views here.
@@ -19,13 +24,16 @@ def meteo_request_view(request):
             cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
             retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
             openmeteo = openmeteo_requests.Client(session=retry_session)
-            geocode_city = requests.get('https://geocoding-api.open-meteo.com/v1/search',
-                                        params={'name': city, 'language': 'ru'}).json()['results']
+            geocode_city_data = requests.get('https://geocoding-api.open-meteo.com/v1/search',
+                                             params={'name': city, 'language': 'ru'})
+            geocode_city = json.loads(geocode_city_data.text)
             weather_responses = openmeteo.weather_api("https://api.open-meteo.com/v1/forecast",
-                                                      {"latitude": geocode_city[0].get('latitude'),
-                                                       "longitude": geocode_city[0].get('longitude'),
+                                                      {"latitude": geocode_city['results'][0].get('latitude'),
+                                                       "longitude": geocode_city['results'][0].get('longitude'),
                                                        "current": "temperature_2m",
-                                                       "hourly": "temperature_2m"})
+                                                       "hourly": "temperature_2m",
+                                                       "forecast_days": 1},
+                                                      )
             response = weather_responses[0]
             current = response.Current()
             current_temperature_2m = current.Variables(0).Value()
@@ -40,11 +48,25 @@ def meteo_request_view(request):
                 freq=pd.Timedelta(seconds=hourly.Interval()),
                 inclusive="left"
             ), "Температура": hourly_temperature_2m.astype(np.int32, copy=False)}
+            hourly_dataframe = pd.DataFrame(data=hourly_data).transpose().to_html(classes="table table-striped")
 
-            hourly_dataframe = pd.DataFrame(data=hourly_data).to_html(classes="table table-striped")
-            return render(request, 'meteo_app/weather.html', {'city': city, 'current_time': current.Time(),
+            if request.user.is_authenticated:
+                new_form = form.save(commit=False)
+                new_form.user = request.user
+                history = UserHistory.objects.filter(user=request.user)
+                new_form.save()
+            else:
+                history = []
+
+            return render(request, 'meteo_app/weather.html', {'city': city,
                                                               'temperature': int(current_temperature_2m),
-                                                              'hourly': hourly_dataframe})
+                                                              'hourly': hourly_dataframe,
+                                                              'history': history})
     else:
         form = MeteoForm()
     return render(request, 'meteo_app/city_form.html', {'form': form})
+
+
+def read_history(request):
+    history = UserHistory.objects.filter(user=request.user)
+    return render(request, 'meteo_app/history.html', {'history': history})
